890223acea83a1bd0f4ea95ae6a1a1434b0cace3
weichunchun_ext <weichun713@foxmail.com>
2018-02-11 17:06:44 +0800

    addauthor

--- src/main/java/com/jsdiuf/jsdiuf/controller/LogController.java
+++ src/main/java/com/jsdiuf/jsdiuf/controller/LogController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author
+ * @author weicc
  */
 @RestController
 @RequestMapping("log")
